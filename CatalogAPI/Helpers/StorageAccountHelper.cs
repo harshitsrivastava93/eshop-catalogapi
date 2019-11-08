dde1a50122061a6004515c5062bebc5b1737bdef
unknown <r1m1nrao@PUNB-TR04-11.hexa.training.local>
2019-11-08 11:47:05 +0530

    first

---
+++ CatalogAPI/Helpers/StorageAccountHelper.cs
@@ -0,0 +1,73 @@
+﻿using CatalogAPI.Models;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage.Table;
+namespace CatalogAPI.Helpers
+{
+    public class StorageAccountHelper
+    {
+        public string storageConnectionString;
+        private string tableConnectionString;
+        private CloudStorageAccount storageAccount;
+        private CloudStorageAccount tableStorageAccount;
+        private CloudBlobClient blobClient;
+        public CloudTableClient tableClient;
+        public string StorageConnectionString
+        {
+            get { return storageConnectionString; }
+            set
+            {
+                this.storageConnectionString = value;
+                storageAccount = CloudStorageAccount.Parse(this.storageConnectionString);
+            }
+        }
+        public string TableConnectionString
+        {
+            get { return tableConnectionString; }
+            set
+            {
+                this.tableConnectionString = value;
+                tableStorageAccount = CloudStorageAccount.Parse(this.tableConnectionString);
+            }
+        }
+        public async Task<string> UploadFileToBlobAsync(string filePath, string containerName)
+        {
+            blobClient = storageAccount.CreateCloudBlobClient();
+            var container = blobClient.GetContainerReference(containerName);
+            await container.CreateIfNotExistsAsync();
+            BlobContainerPermissions permissions = new BlobContainerPermissions()
+            {
+                PublicAccess = BlobContainerPublicAccessType.Container
+            };
+            await container.SetPermissionsAsync(permissions);
+
+            var filename = Path.GetFileName(filePath);
+            var blob = container.GetBlockBlobReference(filename);
+            await blob.DeleteIfExistsAsync();
+            await blob.UploadFromFileAsync(filePath);
+            return blob.Uri.AbsoluteUri;
+        }
+        public async Task<CatalogEntity> SaveToTableAsync(CatalogItem item)
+        {
+            CatalogEntity catalogEntity = new CatalogEntity(item.Name, item.Id)
+            {
+                ImageUrl = item.ImageUrl,
+                ReorderLevel = item.ReorderLevel,
+                Quantity = item.Quantity,
+                Price = item.Price,
+                ManufacturingDate = item.ManufacturingDate
+            };
+            tableClient = tableStorageAccount.CreateCloudTableClient();
+            var catalogTable = tableClient.GetTableReference("catalog");
+            await catalogTable.CreateIfNotExistsAsync();
+            TableOperation operation = TableOperation.InsertOrMerge(catalogEntity);
+            var tableResult = await catalogTable.ExecuteAsync(operation);
+            return tableResult.Result as CatalogEntity;
+        }
+    }
+}
